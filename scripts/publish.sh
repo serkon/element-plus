9f98a30dac80e5533b08040c21f3ebe7da30e06e
Serkan Konakcı <serkan.konakci@timusnetworks.com>
2024-08-26 21:19:33 +0300

    feat: update component sizes and types

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,22 +2,28 @@
 
 set -e
 
+# Ortam değişkenlerini ayarla
+#export TAG_VERSION=$(git describe --tags --abbrev=0)
+
+export TAG_VERSION=$(git describe --tags --abbrev=0 | sed 's/^v//')
+export GIT_HEAD=$(git rev-parse HEAD)
+
 pnpm i --frozen-lockfile
 pnpm update:version
 
 pnpm build
 
 cd dist/element-plus
-npm publish --provenance
-cd -
-
-cd internal/eslint-config
 npm publish
 cd -
 
-cd internal/metadata
-pnpm build
-npm publish
-cd -
+# cd internal/eslint-config
+# npm publish
+# cd -
+#
+# cd internal/metadata
+# pnpm build
+# npm publish
+# cd -
 
 echo "✅ Publish completed"
